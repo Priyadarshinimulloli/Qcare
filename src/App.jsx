92b162432527cc3e2635ee3acbc2c7a29f2013f6
Priyadarshini <priyadarshinimulloli@gmail.com>
2025-10-10 03:03:55 +0530

    changed app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,4 @@
-// import { Routes, Route } from 'react-router-dom'
-// import './App.css'
-// import LandingPage from './components/LandingPage'
-// import Login from './components/Login'
-// import Register from './components/Register'
-// import Home from './components/Home'
-// import AdminDashboard from './components/AdminDashboard'
-// import Analytics from './components/Analytics'
 
-// function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<LandingPage />} />
-//       <Route path="/login" element={<Login />} />
-//       <Route path="/register" element={<Register />} />
-//       <Route path="/home" element={<Home />} />
-//       <Route path="/admin" element={<AdminDashboard />} />
-//       <Route path="/analytics" element={<Analytics />} />
-//     </Routes>
-//   )
-// }
-
-// export default App
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/LandingPage';
